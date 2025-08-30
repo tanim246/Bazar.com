7409a482f2635beb73cf0d93e021adf78a3eb0bd
Tanim <mdtasnimhossaintanim@gmail.com>
2025-08-30 22:57:17 +0600

    add my react codes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Banner from "./components/Banner";
 import Products from "./components/products/products";
 import About from "./Components/Products/About";
 import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+// import Footer from "./Components/shared/Footer";
 
 const App = () => {
   return (
@@ -12,8 +14,8 @@ const App = () => {
       <Navber />
       <Banner />
       <Products />
-
-      {/* <Contact /> */}
+      <Contact />
+      <Footer />
     </div>
   );
 };
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,117 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="px-4 divide-y bg-pink-500 text-white">
+      <div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
+        {/* Brand */}
+        <div className="lg:w-1/3">
+          <a
+            rel="noopener noreferrer"
+            href="#"
+            className="flex justify-center space-x-3 lg:justify-start"
+          >
+            <div className="flex items-center justify-center w-12 h-12 rounded-full bg-pink-600">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 32 32"
+                fill="currentColor"
+                className="flex-shrink-0 w-5 h-5 rounded-full text-white"
+              >
+                <path d="M18.266 26.068l7.839-7.854 4.469 4.479c1.859 1.859 1.859 4.875 0 6.734l-1.104 1.104c-1.859 1.865-4.875 1.865-6.734 0zM30.563 2.531l-1.109-1.104c-1.859-1.859-4.875-1.859-6.734 0l-6.719 6.734-6.734-6.734c-1.859-1.859-4.875-1.859-6.734 0l-1.104 1.104c-1.859 1.859-1.859 4.875 0 6.734l6.734 6.734-6.734 6.734c-1.859 1.859-1.859 4.875 0 6.734l1.104 1.104c1.859 1.859 4.875 1.859 6.734 0l21.307-21.307c1.859-1.859 1.859-4.875 0-6.734z"></path>
+              </svg>
+            </div>
+            <span className="self-center text-2xl font-semibold">
+              Brand name
+            </span>
+          </a>
+        </div>
+
+        {/* Links */}
+        <div className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-4">
+          <div className="space-y-3">
+            <h3 className="tracking-wide uppercase text-white">Product</h3>
+            <ul className="space-y-1">
+              <li>
+                <a href="#">Features</a>
+              </li>
+              <li>
+                <a href="#">Integrations</a>
+              </li>
+              <li>
+                <a href="#">Pricing</a>
+              </li>
+              <li>
+                <a href="#">FAQ</a>
+              </li>
+            </ul>
+          </div>
+          <div className="space-y-3">
+            <h3 className="tracking-wide uppercase text-white">Company</h3>
+            <ul className="space-y-1">
+              <li>
+                <a href="#">Privacy</a>
+              </li>
+              <li>
+                <a href="#">Terms of Service</a>
+              </li>
+            </ul>
+          </div>
+          <div className="space-y-3">
+            <h3 className="uppercase text-white">Developers</h3>
+            <ul className="space-y-1">
+              <li>
+                <a href="#">Public API</a>
+              </li>
+              <li>
+                <a href="#">Documentation</a>
+              </li>
+              <li>
+                <a href="#">Guides</a>
+              </li>
+            </ul>
+          </div>
+          <div className="space-y-3">
+            <div className="uppercase text-white">Social media</div>
+            <div className="flex justify-start space-x-3">
+              {/* Facebook */}
+              <a href="#" className="flex items-center p-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 32 32"
+                  className="w-5 h-5 text-white"
+                >
+                  <path d="M32 16c0-8.839-7.167-16-16-16-8.839 0-16 7.161-16 16 0 7.984 5.849 14.604 13.5 15.803v-11.177h-4.063v-4.625h4.063v-3.527c0-4.009 2.385-6.223 6.041-6.223 1.751 0 3.584 0.312 3.584 0.312v3.937h-2.021c-1.984 0-2.604 1.235-2.604 2.5v3h4.437l-0.713 4.625h-3.724v11.177c7.645-1.199 13.5-7.819 13.5-15.803z"></path>
+                </svg>
+              </a>
+              {/* Twitter */}
+              <a href="#" className="flex items-center p-1">
+                <svg
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-5 h-5 text-white"
+                >
+                  <path d="M23.954 4.569a10 10 0 01-2.825.775 ..."></path>
+                </svg>
+              </a>
+              {/* Instagram */}
+              <a href="#" className="flex items-center p-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 32 32"
+                  fill="currentColor"
+                  className="w-5 h-5 text-black"
+                >
+                  <path d="M16 2.88c4.271 0 4.781 0.021 ..."></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
